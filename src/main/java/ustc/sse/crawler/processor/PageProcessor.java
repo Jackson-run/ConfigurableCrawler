488de89b1eb88cd813d730aceabda36f8a8f2dea
Jackson <1161609893@qq.com>
2017-10-17 21:04:23 +0800

    完善接口设计,定义基础类

--- src/main/java/ustc/sse/crawler/processor/PageProcessor.java
+++ src/main/java/ustc/sse/crawler/processor/PageProcessor.java
@@ -1,11 +1,12 @@
 package ustc.sse.crawler.processor;
 
+import ustc.sse.crawler.Config;
 import ustc.sse.crawler.Response;
 
 /**
  * PageProcessor是Crawler的组件之一实现页面的解析
  * 所有的PageProcessor必须要实现PageProcessor接口
- * 并定制化{@link #process(Response)}方法，实现不同的页面处理逻辑
+ * 并定制化{@link #process(Response, Config)}方法，实现不同的页面处理逻辑
  * @author wangrun
  * @version 0.1
  */
@@ -14,8 +15,9 @@ public interface PageProcessor {
     /**
      * 不同的处理器需定制化此方法，实现不同的页面处理逻辑
      * @param response 传入页面下载器生成的Response对象
+     * @param config 传入配置对象
      */
-    public void process(Response response);
+    public void process(Response response, Config config);
 
 
 }
